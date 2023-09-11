caa42e6fa9f4d167f18fa8a43fe3ce388541b247
sleepingkirby <sleepingkirby@gmail.com>
2023-09-12 02:54:49 +0800

    interm commit

--- js/eventsLib.js
+++ js/eventsLib.js
@@ -16,9 +16,10 @@ CREATE TABLE events(uuid text not null primary key, forUser_id text not null, by
 */
 function createEvent(forUser, byUser, onDt, dur=30, type="service", invntSrvs, users=null){
 let evnt_uuid=createUUID();
-  let query='insert into events(uuid, forUser_id, byUser_id, create_date, on_date, duration) values($uuid, $forUser_id, $byUser_id, datetime("now"), datetime($on_date), $dur)';
+let query='insert into events(uuid, forUser_id, byUser_id, create_date, on_date, duration) values($uuid, $forUser_id, $byUser_id, datetime("now"), datetime($on_date), $dur)';
+let obj={$uuid:evnt_uuid, $forUser_id:forUser, $byUser_id:byUser, $on_date:onDt, $dur:dur};
   try{
-  sqlObj.runQuery(query, {$uuid:evnt_uuid, $forUser_id:forUser, $byUser_id:byUser, $on_date:onDt, $dur:dur});
+  sqlObj.runQuery(query,obj);
   }
   catch(e){
   console.log('Unable to add entry to "events" table. query: '+query+', binds: '+JSON.stringify({$uuid:evnt_uuid, $forUser_id:forUser, $byUser_id:byUser, $on_date:onDt, $dur:dur}));
@@ -30,12 +31,36 @@ let evnt_uuid=createUUID();
 CREATE TABLE events_type(uuid text not null primary key, event_uuid text not null, type_uuid text not null, foreign key(event_uuid) references events(uuid), foreign key(type_uuid) references type(uuid));
 */
   //need to set type for event
+let evntTyp_uuid=createUUID();
+query='insert into events_type(uuid, event_uuid, type_uuid) values($uuid, $event_uuid, (select uuid from type where categ="invntSrv" and name=$type))';
+obj={$uuid:evntTyp_uuid, $event_uuid:evnt_uuid};
+  try{
+  sqlObj.runQuery(query,obj);
+  }
+  catch(e){
+  console.log('Unable to add entry to "events" table. query: '+query+', binds: '+JSON.stringify(obj));
+  console.log(e);
+  return null;
+  }  
+
 
 /*
 CREATE TABLE events_invntSrv(uuid text not null, create_date int not null, events_id text not null, invntSrv_id text not null, foreign key(events_id) references events(uuid), foreign key(invntSrv_id) references invntSrv(uuid));
 */
   //set what services or products are involved in this event
-  
+let evntIvntSrv_uuid=createUUID();
+/* for loop of invntSrvs,
+query='insert into events_type(uuid, event_uuid, type_uuid) values($uuid, $event_uuid, (select uuid from type where categ="invntSrv" and name=$type))';
+obj={$uuid:evntTyp_uuid, $event_uuid:evnt_uuid};
+  try{
+  sqlObj.runQuery(query,obj);
+  }   
+  catch(e){
+  console.log('Unable to add entry to "events" table. query: '+query+', binds: '+JSON.stringify(obj));
+  console.log(e);
+  return null;
+  } 
+  */
 }
 
 
