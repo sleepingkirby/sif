a959a5787d4a4c9235c6e526d730003a1604f424
sleepingkirby <sleepingkirby@gmail.com>
2024-01-10 21:34:28 +0800

    start of the invoices

---
+++ js/invcs.js
@@ -0,0 +1,96 @@
+if(typeof invcs==='undefined'){
+/*-------------------------------------------
+pre: global mainObj, sqlObj, state variable, eventsLib for event/appointment CRUD
+post: html changed, db updated
+manage inventory and services
+-------------------------------------------*/
+  class invcs{
+    constructor(){
+      this.tmpl={};
+    this.tmpl.headers=`
+      <div id="invcAdd" class="moduleAdd">
+        <div id="invcAddBtn" class="moduleAddBtn" title="New Invoice" tabindex=0>⨁</div>
+      </div>
+      <div id="invcFltr" class="fltrRow">
+        <div id="invcFltrInptWrap" class="fltrRowCell">
+          <input id="invcFltrInpt" name="invcFilter[input]" class="fltrInpt" type="text" placeholder="Filter by name, price, amnts, duration, sku" title="Filter by name, price, amnts, duration, sku"/>
+        </div>
+        <div class="">
+          <div class="fltrRowCellLbl">
+          Status Filter:
+          </div>
+          <select id="invcFilterStatus" name="invcFilter[status]" class="fltrSlct" title="Status Filter">
+          <option>all</option>
+          </select>
+        </div>
+        <div class="">
+          <div class="fltrRowCellLbl">
+          Type Filter:
+          </div>
+          <select id="invcFilterType" name="invcFilter[type]" class="fltrSlct" title="Type Filter">
+          <option>all</option>
+          </select>
+        </div>
+      </div>
+      <div id="invcMain" class="moduleTblMain">
+      </div>
+    `;
+    }
+
+    /*-----------------------------------------------
+    pre: none
+    post: draws top left nav
+    draws top left nav
+    -----------------------------------------------*/
+    genLeftNavInvntSrv(){
+    return "Invoices";
+    }
+
+    /*----------------------------------
+    pre: apptAddBtn element exists, mainObj.modPrcClsCall()
+    post: event hook added
+    addes event hook to apptAddBtn element
+    ----------------------------------*/
+    hookNewInvntBtn(){
+      document.getElementById("invcAddBtn").onclick=(e)=>{
+      let el=document.getElementById('rghtMod').getElementsByClassName("close")[0];
+      mainObj.modPrcClsCall(el);
+      };
+    }
+
+    /*----------------------------------
+    pre: everything this class requires
+    post: events added to elements.
+    adds event hooks to elements that need it
+    ----------------------------------*/
+    hookEl(){
+    this.hookNewInvntBtn();
+    }
+
+
+    /*-----------------------------------------------
+    pre: none
+    post: write the html to the page
+    draw basics to page mainEl
+    -----------------------------------------------*/
+    mainEl(){
+    let html=this.tmpl.headers;
+    return html;
+    }
+
+
+
+    /*-----------------------------------------------
+    pre:
+    post:
+    -----------------------------------------------*/
+    run(){
+    document.getElementById('leftNavMod').innerHTML=this.genLeftNavInvntSrv();
+    document.getElementById('mainEl').innerHTML=this.mainEl();
+    this.hookNewInvntBtn()
+    }
+  }
+
+var invcsObj=new invcs();
+state.depModuleObjs['invcs']=invcsObj;
+}
